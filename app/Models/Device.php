4de2fa7c003aa3cc1b8b4dce785cc975a13651c7
masterpoku <wafitakputusasa@gmail.com>
2024-10-22 14:28:56 +0700

    device view

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -2,20 +2,51 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+use Scaffolding\Traits\ScaffoldingModel;
 
-class Device extends Model
+class Device extends Authenticatable implements JWTSubject
 {
-    protected $fillable = ['device_name', 'device_code', 'ip'];
+    use HasFactory, Notifiable, HasRoles, ScaffoldingModel;
 
-    // Define the fields() method
-    public static function fields()
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'device_name',
+        'device_code',
+        'ip',
+    ];
+
+    public function __construct(array $attributes = [])
     {
-        return [
-            'device_name' => 'Device Name',
-            'device_code' => 'Device Code',
-            'ip' => 'IP Address',
-            // Add other fields as needed
-        ];
+        $this->initialize();
+        parent::__construct($attributes);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
